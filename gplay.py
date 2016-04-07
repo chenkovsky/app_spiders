607b93fcf296267a61e2a672d2d2fc5b4fd4b604
chenkovsky <chenkov@yeah.net>
2016-04-07 11:15:01 +0800

    fix bug

--- gplay.py
+++ gplay.py
@@ -49,12 +49,7 @@ class GPlaySpider(CrawlSpider):
 
     def header(self):
         return {
-            #"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            #"Connection":"keep-alive",
-            #"Accept-Encoding":"gzip, deflate, sdch",
-            #"Accept-Language":"zh-CN,zh;q=0.8,en;q=0.6",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-            #"Cookie":"hxck_sq_common=LoginStateCookie=; Hm_lvt_cb1b8b99a89c43761f616e8565c9107f=1422013312,1422070860; Hm_lpvt_cb1b8b99a89c43761f616e8565c9107f=1422070860; __utma=232688221.1600897606.1422070864.1422070864.1422070864.1; __utmc=232688221; __utmz=232688221.1422070864.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic|utmctr=%E8%82%A1%E7%A5%A8%E5%88%97%E8%A1%A8%20hexun; ASP.NET_SessionId=uofawnqc5404mx455ocaif55; vjuids=2e5d1a4f.14b1696e9ec.0.01e018a5; hxck_webdev1_general=stocklist=600216_1; __utma=194262068.170830345.1422013312.1422070864.1422075855.3; __utmc=194262068; __utmz=194262068.1422075855.3.3.utmcsr=so.hexun.com|utmccn=(referral)|utmcmd=referral|utmcct=/default.do; vjlast=1422013164.1422070859.13; HexunTrack=SID=2015012319392701377959271ecde4f0bbf60c73654e82ee4&CITY=31&TOWN=0"
         }
 
     def url(self, package_name): return self.url_template % package_name
